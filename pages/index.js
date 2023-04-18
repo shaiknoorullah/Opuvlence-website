76d5e14928349c446c47c837ff3fde928615a87a
shaiknoorullah <shaiknooru247@gmail.com>
2023-04-18 22:04:34 +0530

    template gen

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,36 @@
 import Head from "next/head"
 import Image from "next/image"
 import styles from "../styles/Home.module.css"
+import { useEffect, useRef } from "react"
+import { gsap } from "gsap"
 
 export default function Home() {
+	const mainRef = useRef(null)
+
+	useEffect(() => {
+		if (mainRef.current) {
+			const mainRefVar = mainRef.current.children
+
+			gsap.fromTo(
+				mainRefVar,
+				{
+					y: -10,
+					opacity: 0,
+				},
+				{
+					y: 0,
+					opacity: 1,
+					duration: 0.5,
+					delay: 0.5,
+					stagger: 0.2,
+					ease: "power2.out",
+				}
+			)
+		}
+	}, [])
+
 	return (
-		<div className={styles.container}>
+		<div>
 			<Head>
 				<title>Create Next App</title>
 				<meta
@@ -19,86 +45,15 @@ export default function Home() {
 				/>
 			</Head>
 
-			<main className={styles.main}>
-				<h1 className={styles.title}>
-					Welcome to{" "}
-					<a href="https://nextjs.org">Next.js!</a>
+			<main
+				ref={mainRef}
+				className="w-screen min-h-screen flex justify-center items-center"
+			>
+				<h1 className="text-[8vw] text-white font-bold">
+					This is an auto generated next js template for
+					opuvlence
 				</h1>
-				<h1 className="text-3xl font-bold underline">
-					Hello world!
-				</h1>
-
-				<p className={styles.description}>
-					Get started by editing{" "}
-					<code className={styles.code}>
-						pages/index.js
-					</code>
-				</p>
-
-				<div className={styles.grid}>
-					<a
-						href="https://nextjs.org/docs"
-						className={styles.card}
-					>
-						<h2>Documentation &rarr;</h2>
-						<p>
-							Find in-depth information about Next.js
-							features and API.
-						</p>
-					</a>
-
-					<a
-						href="https://nextjs.org/learn"
-						className={styles.card}
-					>
-						<h2>Learn &rarr;</h2>
-						<p>
-							Learn about Next.js in an interactive course
-							with quizzes!
-						</p>
-					</a>
-
-					<a
-						href="https://github.com/vercel/next.js/tree/canary/examples"
-						className={styles.card}
-					>
-						<h2>Examples &rarr;</h2>
-						<p>
-							Discover and deploy boilerplate example
-							Next.js projects.
-						</p>
-					</a>
-
-					<a
-						href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-						className={styles.card}
-					>
-						<h2>Deploy &rarr;</h2>
-						<p>
-							Instantly deploy your Next.js site to a public
-							URL with Vercel.
-						</p>
-					</a>
-				</div>
 			</main>
-
-			<footer className={styles.footer}>
-				<a
-					href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Powered by{" "}
-					<span className={styles.logo}>
-						<Image
-							src="/vercel.svg"
-							alt="Vercel Logo"
-							width={72}
-							height={16}
-						/>
-					</span>
-				</a>
-			</footer>
 		</div>
 	)
 }
