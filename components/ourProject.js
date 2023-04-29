295dea9e9d5387665eb1cc9c9e6ed7fa5fe52916
shaiknoorullah <shaiknooru247@gmail.com>
2023-04-29 21:57:04 +0530

    temp checkout

--- components/ourProject.js
+++ components/ourProject.js
@@ -1,14 +1,11 @@
-<<<<<<<<< Temporary merge branch 1
 /** @format */
 
 import react from "react"
 import localFont from "next/font/local"
-import react from "react";
-import localFont from "next/font/local";
-import ScrollContainer from "react-indiana-drag-scroll";
-import { useEffect } from "react";
-import { gsap } from "gsap";
-import Link from "next/link";
+import ScrollContainer from "react-indiana-drag-scroll"
+import { useEffect } from "react"
+import { gsap } from "gsap"
+import Link from "next/link"
 
 const golden = localFont({
 	src: "../styles/font/golden/golden.woff2",
@@ -19,120 +16,121 @@ const poppins = localFont({
 	variable: "--font-poppins",
 })
 
-const Residential = () => {
+// const Residential = () => {
+// 	return (
+// 		<div className="flex flex-row relative w-full 3xl:gap-[12px] lg:gap-[0.625vw]">
+// 			{/* <div className="flex flex-row  absolute bottom-[36px] left-[-200px]"> */}
+// 			<img
+// 				src="ourproject.png"
+// 				className="w-[25vw]"
+// 			></img>
+// 			<div className="flex flex-col">
+// 				<p className="font-extrabold 3xl:text-[25px] lg:text-[1.3vw] 3xl:leading-[37.5px] lg:leading-[1.95vw] tracking-[0.5%] mt-[-9px]">
+// 					2020
+// 				</p>
+// 				<p className="font-medium 3xl:text-[89.14px] lg:text-[4.642vw] 3xl:leading-[133.71px] lg:leading-[6.93vw] tracking-[0.5%] rotate-[270deg] absolute 3xl:left-[328px] lg:left-[16.5vw] 3xl:mt-[228px] lg:mt-[11.875vw] uppercase">
+// 					{" "}
+// 					Residential
+// 				</p>
+// 			</div>
+// 			{/* </div> */}
+// 		</div>
+// 	)
+// }
+// const OurProject = () => {
+// 	return (
+// 		<div
+// 			// data-scroll-section
+// 			className="w-full max-w-[1920px] items-center flex justify-center "
+// 		>
+// 			<div className="font-poppins w-[98%] z-50 ">
+// 				<div className="w-[65%] justify-center font-extrabold text-[1.3rem] text-[#A5787A] flex pt-[20px]">
+// 					/ OUR PROJECTS
+// 				</div>
+// 				<div className="flex flex-row 3xl:gap-[168px] lg:gap-[8.75vw]">
+// 					<text className="3xl:w-[583px] lg:w-[30.36vw]  3xl:text-[33px] lg:text-[1.71vw]  3xl:leading-[45.34px] lg:leading-[2.361vw] tracking-[10%] font-bold 3xl:pl-[50px] 3xl:pt-[41px] lg:pl-[2.60vw] lg:pt-[2.13vw] snap-center font-[lamore]">
+// 						WE&apos;VE BUILT HOMES THAT BRING DREAMS COME
+// 						TRUE
+// 					</text>
+// 					<div className="3xl:w-[442px] lg:w-[23.02vw] 3xl:text-[16px] lg:text-[0.833vw] 3xl:leading-[24.85px] lg:leading-[1.294vw] tracking-[7%] font-normal  3xl:pt-[86px] lg:pt-[4.47vw]  snap-center">
+// 						Using edge cutting technology to provide
+// 						extremely reliable service Using edge cutting
+// 						technology to provide extremely reliable service
+// 						Using edge cutting technology to provide
+// 						extremely reliable serviceUsing edge cutting
+// 						technology to provide extremely reliable service
+// 					</div>
+// 				</div>
+// 				{/* reuse */}
+// 				<div className="3xl:w-[587px] lg:w-[30.57vw] 3xl:pl-[50px] lg:pl-[2.60vw]  relative pb-[10px]">
+// 					<div className="flex flex-row items-center 3xl:gap-[15px] lg:gap-[0.78vw] absolute 3xl:top-[-13.59px] lg:top-[-0.708vw]">
+// 						<div className="3xl:text-[22.15px]  3xl:leading-[33.22px] lg:text-[1.153vw] lg:leading-[1.73vw] tracking-[0.5%] font-medium italic uppercase">
+// 							residential
+// 						</div>
+// 						<img
+// 							src="OurprojectVector.png"
+// 							className=" 3xl:h-[12.17px] 3xl:w-[64.78px] lg:h-[0.633vw] lg:w-[3.37vw]"
+// 						></img>
+// 						<div className="3xl:text-[22.15px] 3xl:leading-[33.22px] lg:text-[1.153vw] lg:leading-[1.73vw] tracking-[0.5%]  italic uppercase">
+// 							commercial
+// 						</div>
+// 						<img
+// 							src="OurprojectVector.png"
+// 							className="3xl:h-[12.17px] 3xl:w-[64.78px] lg:h-[0.633vw] lg:w-[3.37vw]  ml-1"
+// 						></img>
+// 						<div className="3xl:text-[22.15px]  3xl:leading-[33.22px] lg:text-[1.153vw] lg:leading-[1.73vw] tracking-[0.5%]  italic uppercase">
+// 							restaurant
+// 						</div>
+// 					</div>
+// 				</div>
+// 				<div className="grid w-[700px] h-[550px] grid-cols-[0.3fr_2.2fr_0.7fr_1.9fr_0.5fr_0.4fr] grid-rows-[0.1fr_1.6fr_0.8fr]">
+//           <div
+//             style={{
+//               gridArea: "2 / 1 / 3 / 5",
+//             }}
+//             className={` ${golden.className} pointer-events-none select-none w-[792px]   3xl:text-[188px] lg:text-[9.79vw] lg:leading-[9.79vw] 3xl:leading-[188px] tracking-[3%] font-normal font-[gilroy] uppercase mt-0 z-[10]`}
+//           >
+//             Bonito Designs
+//           </div>
+//           <div
+//             style={{
+//               gridArea: "3 / 2 / 4 / 4",
+//             }}
+//             className="grid-cols-1 3xl:text-[16px] lg:text-[0.833vw] 3xl:leading-[24.85px] ml-8 mr-[-3.6rem] lg:leading-[1.29vw] tracking-[7%] font-normal z-[10]"
+//           >
+const Residential = props => {
 	return (
 		<div className="flex flex-row relative w-full 3xl:gap-[12px] lg:gap-[0.625vw]">
-			{/* <div className="flex flex-row  absolute bottom-[36px] left-[-200px]"> */}
 			<img
-				src="ourproject.png"
-				className="w-[25vw]"
+				src={props.src}
+				className="3xl:w-[558px] lg:w-[29.06vw] 3xl:h-[558px] lg:h-[29vw] "
 			></img>
 			<div className="flex flex-col">
 				<p className="font-extrabold 3xl:text-[25px] lg:text-[1.3vw] 3xl:leading-[37.5px] lg:leading-[1.95vw] tracking-[0.5%] mt-[-9px]">
-					2020
+					{props.date}
 				</p>
-				<p className="font-medium 3xl:text-[89.14px] lg:text-[4.642vw] 3xl:leading-[133.71px] lg:leading-[6.93vw] tracking-[0.5%] rotate-[270deg] absolute 3xl:left-[328px] lg:left-[16.5vw] 3xl:mt-[228px] lg:mt-[11.875vw] uppercase">
-					{" "}
-					Residential
+				<p className="font-medium  3xl:text-[87.14px] lg:text-[4.642vw] 3xl:leading-[133.71px] lg:leading-[6.93vw] tracking-[0.5%] rotate-[270deg] absolute 3xl:left-[328px] lg:left-[16.5vw] 3xl:mt-[228px] lg:mt-[11.875vw] uppercase">
+					{props.type}
 				</p>
 			</div>
-			{/* </div> */}
 		</div>
 	)
 }
-const OurProject = () => {
-	return (
-		<div
-			// data-scroll-section
-			className="w-full max-w-[1920px] items-center flex justify-center "
-		>
-			<div className="font-poppins w-[98%] z-50 ">
-				<div className="w-[65%] justify-center font-extrabold text-[1.3rem] text-[#A5787A] flex pt-[20px]">
-					/ OUR PROJECTS
-				</div>
-				<div className="flex flex-row 3xl:gap-[168px] lg:gap-[8.75vw]">
-					<text className="3xl:w-[583px] lg:w-[30.36vw]  3xl:text-[33px] lg:text-[1.71vw]  3xl:leading-[45.34px] lg:leading-[2.361vw] tracking-[10%] font-bold 3xl:pl-[50px] 3xl:pt-[41px] lg:pl-[2.60vw] lg:pt-[2.13vw] snap-center font-[lamore]">
-						WE&apos;VE BUILT HOMES THAT BRING DREAMS COME
-						TRUE
-					</text>
-					<div className="3xl:w-[442px] lg:w-[23.02vw] 3xl:text-[16px] lg:text-[0.833vw] 3xl:leading-[24.85px] lg:leading-[1.294vw] tracking-[7%] font-normal  3xl:pt-[86px] lg:pt-[4.47vw]  snap-center">
-						Using edge cutting technology to provide
-						extremely reliable service Using edge cutting
-						technology to provide extremely reliable service
-						Using edge cutting technology to provide
-						extremely reliable serviceUsing edge cutting
-						technology to provide extremely reliable service
-					</div>
-				</div>
-				{/* reuse */}
-				<div className="3xl:w-[587px] lg:w-[30.57vw] 3xl:pl-[50px] lg:pl-[2.60vw]  relative pb-[10px]">
-					<div className="flex flex-row items-center 3xl:gap-[15px] lg:gap-[0.78vw] absolute 3xl:top-[-13.59px] lg:top-[-0.708vw]">
-						<div className="3xl:text-[22.15px]  3xl:leading-[33.22px] lg:text-[1.153vw] lg:leading-[1.73vw] tracking-[0.5%] font-medium italic uppercase">
-							residential
-						</div>
-						<img
-							src="OurprojectVector.png"
-							className=" 3xl:h-[12.17px] 3xl:w-[64.78px] lg:h-[0.633vw] lg:w-[3.37vw]"
-						></img>
-						<div className="3xl:text-[22.15px] 3xl:leading-[33.22px] lg:text-[1.153vw] lg:leading-[1.73vw] tracking-[0.5%]  italic uppercase">
-							commercial
-						</div>
-						<img
-							src="OurprojectVector.png"
-							className="3xl:h-[12.17px] 3xl:w-[64.78px] lg:h-[0.633vw] lg:w-[3.37vw]  ml-1"
-						></img>
-						<div className="3xl:text-[22.15px]  3xl:leading-[33.22px] lg:text-[1.153vw] lg:leading-[1.73vw] tracking-[0.5%]  italic uppercase">
-							restaurant
-						</div>
-					</div>
-				</div>
-				{/* <div className="grid w-[700px] h-[550px] grid-cols-[0.3fr_2.2fr_0.7fr_1.9fr_0.5fr_0.4fr] grid-rows-[0.1fr_1.6fr_0.8fr]">
-          <div
-            style={{
-              gridArea: "2 / 1 / 3 / 5",
-            }}
-            className={` ${golden.className} pointer-events-none select-none w-[792px]   3xl:text-[188px] lg:text-[9.79vw] lg:leading-[9.79vw] 3xl:leading-[188px] tracking-[3%] font-normal font-[gilroy] uppercase mt-0 z-[10]`}
-          >
-            Bonito Designs
-          </div>
-          <div
-            style={{
-              gridArea: "3 / 2 / 4 / 4",
-            }}
-            className="grid-cols-1 3xl:text-[16px] lg:text-[0.833vw] 3xl:leading-[24.85px] ml-8 mr-[-3.6rem] lg:leading-[1.29vw] tracking-[7%] font-normal z-[10]"
-          >
-=========
-const Residential = (props) => {
-  return (
-    <div className="flex flex-row relative w-full 3xl:gap-[12px] lg:gap-[0.625vw]">
-      <img
-        src={props.src}
-        className="3xl:w-[558px] lg:w-[29.06vw] 3xl:h-[558px] lg:h-[29vw] "
-      ></img>
-      <div className="flex flex-col">
-        <p className="font-extrabold 3xl:text-[25px] lg:text-[1.3vw] 3xl:leading-[37.5px] lg:leading-[1.95vw] tracking-[0.5%] mt-[-9px]">
-          {props.date}
-        </p>
-        <p className="font-medium  3xl:text-[87.14px] lg:text-[4.642vw] 3xl:leading-[133.71px] lg:leading-[6.93vw] tracking-[0.5%] rotate-[270deg] absolute 3xl:left-[328px] lg:left-[16.5vw] 3xl:mt-[228px] lg:mt-[11.875vw] uppercase">
-          {props.type}
-        </p>
-      </div>
-    </div>
-  );
-};
 
-const Grid = (props) => {
-  return (
-    <div className="flex flex-col row-span-full col-start-1 col-end-7 row-start-1 row-end-3 z-40">
-      <div
-        className={` ${golden.className} pointer-events-none select-none   3xl:text-[188px] lg:text-[9.79vw] lg:leading-[9.79vw] 3xl:leading-[188px] tracking-[0.03em] font-normal font-[gilroy] uppercase 3xl:mt-[15px] lg:mt-[0.78vw] z-[10] `}
-      >
-        {props.tittle}
-      </div>
-      <div className="font-medium  ml-[3.5rem] z-30">{props.discription}</div>
-    </div>
-  );
-};
+const Grid = props => {
+	return (
+		<div className="flex flex-col row-span-full col-start-1 col-end-7 row-start-1 row-end-3 z-40">
+			<div
+				className={` ${golden.className} pointer-events-none select-none   3xl:text-[188px] lg:text-[9.79vw] lg:leading-[9.79vw] 3xl:leading-[188px] tracking-[0.03em] font-normal font-[gilroy] uppercase 3xl:mt-[15px] lg:mt-[0.78vw] z-[10] `}
+			>
+				{props.tittle}
+			</div>
+			<div className="font-medium  ml-[3.5rem] z-30">
+				{props.discription}
+			</div>
+		</div>
+	)
+}
 const OurProject = () => {
 	useEffect(() => {
 		const viewMoreButtonRef = document.getElementById(
