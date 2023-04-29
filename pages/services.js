15cd9cd26ab6ab8d1f2145565e49dddb1491eb9e
aziz <coding2537@gmail.com>
2023-04-30 02:09:03 +0530

    services mobile completed

--- pages/services.js
+++ pages/services.js
@@ -5,18 +5,17 @@ import Navbar from "../components/Navbar";
 import TransitionScreen from "../components/TransitionScreen";
 import Services4 from "../components/Services4";
 import ServicesBottom from "../components/ServicesBottom";
+import ServicesBottomMobile from "../components/ServicesBottomMobile";
 
 const Services = () => {
   return (
-    <div className="bg-[#F5E1E2] w-full h-screen px-[40px] ">
+    <div className="bg-[#F5E1E2] w-full h-screen lg:px-[40px] ">
       <TransitionScreen />
       <Navbar />
-      {/* pls remove services4  , servicescomp and ourservices  */}
-      {/* <OurServices /> */}
-      <ServicesBottom />
-      {/* <ServicesComp /> */}
-      <Services4 />
-      <ServicesBottom />
+      {/* <ServicesBottom /> */}
+      <ServicesBottomMobile />
+
+      {/* <Services4 /> */}
     </div>
   );
 };
