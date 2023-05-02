ec8c1c78d4162c4de51bff694dacaaaa108b0a66
ayan <ayan.websleak@gmail.com>
2023-05-02 18:22:33 +0530

    ayan commit - images

--- components/Imgs4.js
+++ components/Imgs4.js
@@ -1,71 +1,58 @@
 /** @format */
 
-import React from "react"
+import React from "react";
+import Image from "next/image";
+import Firstimg from "../public/homepage/Living1.webp";
 
 const Imgs4 = () => {
-	return (
-		<div
-			data-scroll
-			data-scroll-speed="0.5"
-			className="w-full flex base:mt-0 md:mt-[-150px] lg:justify-center"
-		>
-			<section className="w-[95%] max-w-[1920px] base:hidden lg:flex justify-center ">
-				<div className=" w-full grid grid-cols-9  grid-rows-6  ">
-					<div
-						data-scroll
-						data-scroll-speed="1.5"
-						className=" mr-[6rem] z-[1000]  row-span-full row-start-3 row-end-6 col-start-1 col-end-4"
-					>
-						<img
-							src="/img1.png"
-							className=" "
-						/>
-					</div>
+  return (
+    <div
+      data-scroll
+      data-scroll-speed="0.5"
+      className="w-full flex base:mt-0 md:mt-[-150px] lg:justify-center"
+    >
+      <section className="w-[95%] max-w-[1920px] base:hidden lg:flex justify-center ">
+        <div className=" w-full grid grid-cols-9  grid-rows-6  ">
+          <div
+            data-scroll
+            data-scroll-speed="1.5"
+            className=" mr-[6rem] z-[1000]  row-span-full row-start-3 row-end-6 col-start-1 col-end-4"
+          >
+            <img src="/img1.png" className=" " />
+          </div>
 
-					<div
-						data-scroll
-						data-scroll-speed="2"
-						className="row-span-full z-[100]  row-start-1 row-end-5  col-start-3 col-end-6 "
-					>
-						<img
-							src="/img2.png"
-							className=" "
-						/>
-					</div>
+          <div
+            data-scroll
+            data-scroll-speed="2"
+            className="row-span-full z-[100]  row-start-1 row-end-5  col-start-3 col-end-6 "
+          >
+            <img src="/img2.png" className=" " />
+          </div>
 
-					<div
-						data-scroll
-						data-scroll-speed="2.5"
-						className="col-start-4 col-end-8 mt-[2rem] ml-[9rem] mr-[-8rem]  z-[1] row-start-3 row-end-7   row-span-full"
-					>
-						<img
-							src="/img3.png"
-							className=""
-						/>
-					</div>
+          <div
+            data-scroll
+            data-scroll-speed="2.5"
+            className="col-start-4 col-end-8 mt-[2rem] ml-[9rem] mr-[-8rem]  z-[1] row-start-3 row-end-7   row-span-full"
+          >
+            <img src="/img3.png" className="" />
+          </div>
 
-					<div
-						data-scroll
-						data-scroll-speed="3"
-						className=" col-start-7 col-end-13 row-start-2 row-end-5 z-10 ml-[7rem] row-span-full"
-					>
-						<img
-							src="/img4.png"
-							className=" "
-						/>
-					</div>
-				</div>
-			</section>
+          <div
+            data-scroll
+            data-scroll-speed="3"
+            className=" col-start-7 col-end-13 row-start-2 row-end-5 z-10 ml-[7rem] row-span-full"
+          >
+            <img src="/img4.png" className=" " />
+          </div>
+        </div>
+      </section>
 
-			{/* for mobile */}
-			<div className="w-full lg:hidden mt-16">
-				<img
-					className="w-full"
-					src="/imagesection.jpg"
-				/>
-			</div>
-		</div>
-	)
-}
+      {/* for mobile */}
+      <div className="w-full lg:hidden mt-16">
+        <img className="w-full" src="/imagesection.jpg" />
+      </div>
+    </div>
+  );
+};
 
-export default Imgs4
+export default Imgs4;
--- components/testimonials.js
+++ components/testimonials.js
@@ -1,221 +1,202 @@
 /** @format */
 
-import React, { useState } from "react"
-import CustomButton from "./section/customButton"
-import ScrollContainer from "react-indiana-drag-scroll"
-import { golden, poppins } from "../utils/fonts"
+import React, { useState } from "react";
+import CustomButton from "./section/customButton";
+import ScrollContainer from "react-indiana-drag-scroll";
+import { golden, poppins } from "../utils/fonts";
 
-import localFont from "next/font/local"
+import localFont from "next/font/local";
 
 const poppinsBold = localFont({
-	src: "../styles/font/poppins/Poppins-ExtraBold.woff2",
-	variable: "--font-poppins",
-})
+  src: "../styles/font/poppins/Poppins-ExtraBold.woff2",
+  variable: "--font-poppins",
+});
 
-const TestimonialCard = ({ text, description, image }) => {}
+const TestimonialCard = ({ text, description, image }) => {};
 
 const Testimonials = () => {
-	const [showModal, setShowModal] = useState(false)
-	const cards = [
-		{
-			title: "CAROLYN",
-			img: "/testimonial.png",
-			description:
-				"Using edge cutting technology to provide extremely reliable service Using edge cutting technology to provide extremely reliable service Using edge cutting technology to provide extremely reliable serviceUsing edge cutting technology to provide extremely reliable service",
-		},
-		{
-			title: "CAROLYN",
-			img: "/testimonial.png",
-			description:
-				"Using edge cutting technology to provide extremely reliable service Using edge cutting technology to provide extremely reliable service Using edge cutting technology to provide extremely reliable serviceUsing edge cutting technology to provide extremely reliable service",
-		},
-		{
-			title: "CAROLYN",
-			img: "/testimonial.png",
-			description:
-				"Using edge cutting technology to provide extremely reliable service Using edge cutting technology to provide extremely reliable service Using edge cutting technology to provide extremely reliable serviceUsing edge cutting technology to provide extremely reliable service",
-		},
-	]
+  const [showModal, setShowModal] = useState(false);
+  const cards = [
+    {
+      title: "CAROLYN",
+      img: "/testimonial.png",
+      description:
+        "Using edge cutting technology to provide extremely reliable service Using edge cutting technology to provide extremely reliable service Using edge cutting technology to provide extremely reliable serviceUsing edge cutting technology to provide extremely reliable service",
+    },
+    {
+      title: "CAROLYN",
+      img: "/testimonial.png",
+      description:
+        "Using edge cutting technology to provide extremely reliable service Using edge cutting technology to provide extremely reliable service Using edge cutting technology to provide extremely reliable serviceUsing edge cutting technology to provide extremely reliable service",
+    },
+    {
+      title: "CAROLYN",
+      img: "/testimonial.png",
+      description:
+        "Using edge cutting technology to provide extremely reliable service Using edge cutting technology to provide extremely reliable service Using edge cutting technology to provide extremely reliable serviceUsing edge cutting technology to provide extremely reliable service",
+    },
+  ];
 
-	return (
-		<div className="w-full flex  text-black my-[8rem]  justify-center relative">
-			<div className="font-poppins base:w-[95%] lg:w-[100%] z-50 flex flex-col py-12">
-				{/* testimonials title */}
-				<div className="flex w-full base:justify-center lg:justify-start">
-					<div className="lg:inline-block hidden w-[30%]"></div>
+  return (
+    <div className="w-full flex  text-black my-[8rem]  justify-center relative">
+      <div className="font-poppins base:w-[95%] lg:w-[100%] z-50 flex flex-col py-12">
+        {/* testimonials title */}
+        <div className="flex w-full base:justify-center lg:justify-start">
+          <div className="lg:inline-block hidden w-[30%]"></div>
 
-					<div
-						className={`font-[700] text-[1.2rem] text-[#A5787A] flex ${poppinsBold.className}`}
-					>
-						/ TESTIMONIALS
-					</div>
-				</div>
-				{/* Lets see what our */}
-				<div
-					className={`lg:w-[30%] ${poppins} text-black base:text-[1.5rem] lg:text-[1.8rem] leading-[2.4rem] lg:ml-10 font-[700] pt-8`}
-				>
-					LETS SEE WHAT OUR CUSTOMERS HAVE TO SAY
-				</div>
-				{/* cards desktop*/}
-				<div className="scrollbar-hide  overflow-y-scroll hidden cursor-grab lg:flex gap-[4rem] ">
-					<ScrollContainer
-						style={{
-							overflow: "scroll",
-							display: "flex",
-							gap: "4rem",
-						}}
-					>
-						{cards.map((data, index) => {
-							return (
-								<div
-									data-scroll
-									data-scroll-direction="horizontal"
-									data-scroll-speed="1.5"
-									data-scroll-draggable="true"
-									key={index}
-									className="flex flex-col min-w-[68rem] pl-[7rem] mr-[2rem]"
-								>
-									{showModal && (
-										<div
-											className={
-												showModal
-													? `fixed top-0 left-0 right-0 bottom-0 bg-gray-500 bg-opacity-50 z-[1000] flex justify-center items-center `
-													: `hidden`
-											}
-											style={{ overflowY: "hidden" }}
-										>
-											<div className="bg-white rounded-lg p-8 w-[50rem]">
-												{/* Image */}
-												<img
-													src={data.img}
-													alt="Modal Image"
-													className="mx-auto mb-4"
-												/>
-												{/* Text */}
-												<p className="text-gray-700 mb-4">
-													{data.description}
-												</p>
-												{/* Close button */}
-												<button
-													className="bg-[#270405] hover:bg-black text-white font-bold py-2 px-4 rounded"
-													onClick={() =>
-														setShowModal(false)
-													}
-												>
-													Close
-												</button>
-											</div>
-										</div>
-									)}
-									<div>
-										<div
-											className={`text-[6rem] w-full pl-[25rem] tracking-[1rem] italic text-black ${poppins}`}
-										>
-											{data.title}
-										</div>
-										<div className="flex gap-[4rem]">
-											<img
-												onClick={() => setShowModal(true)}
-												className="min-w-[33rem] h-[10rem]"
-												src={data.img}
-											/>
-											<div
-												className={`min-w-[18rem] text-[0.8rem] leading-[30px] self-end ${poppins}`}
-											>
-												{data.description}
-											</div>
-										</div>
-									</div>
-								</div>
-							)
-						})}
-					</ScrollContainer>
-				</div>
-				<div className="scrollbar-hide overflow-y-scroll  flex gap-3 ">
-					{cards.map((data, index) => {
-						return (
-							<div
-								className=" flex lg:hidden min-w-[min(22rem,100vw)] flex-col justify-center mt-12 "
-								key={index}
-							>
-								{showModal && (
-									<div
-										className={
-											showModal
-												? `fixed top-0 left-0 right-0 bottom-0 bg-gray-500 bg-opacity-50 z-[1000] flex justify-center items-center `
-												: `hidden`
-										}
-										style={{ overflowY: "hidden" }}
-									>
-										<div className="bg-white rounded-lg base:p-3 lg:p-8 w-[50rem]">
-											{/* Image */}
-											<img
-												src="/testimonial.png"
-												alt="Modal Image"
-												className="mx-auto mb-4"
-											/>
+          <div
+            className={`font-[700] text-[1.2rem] text-[#A5787A] flex ${poppinsBold.className}`}
+          >
+            / TESTIMONIALS
+          </div>
+        </div>
+        {/* Lets see what our */}
+        <div
+          className={`lg:w-[30%] ${poppins} text-black base:text-[1.5rem] lg:text-[1.8rem] leading-[2.4rem] lg:ml-10 font-[700] pt-8`}
+        >
+          LETS SEE WHAT OUR CUSTOMERS HAVE TO SAY
+        </div>
+        {/* cards desktop*/}
+        <div className="scrollbar-hide  overflow-y-scroll hidden cursor-grab lg:flex gap-[4rem] ">
+          <ScrollContainer
+            style={{
+              overflow: "scroll",
+              display: "flex",
+              gap: "4rem",
+            }}
+          >
+            {cards.map((data, index) => {
+              return (
+                <div
+                  data-scroll
+                  data-scroll-direction="horizontal"
+                  data-scroll-speed="1.5"
+                  data-scroll-draggable="true"
+                  key={index}
+                  className="flex flex-col min-w-[68rem] pl-[7rem] mr-[2rem]"
+                >
+                  {showModal && (
+                    <div
+                      className={
+                        showModal
+                          ? `fixed top-0 left-0 right-0 bottom-0 bg-gray-500 bg-opacity-50 z-[1000] flex justify-center items-center `
+                          : `hidden`
+                      }
+                      style={{ overflowY: "hidden" }}
+                    >
+                      <div className="bg-white rounded-lg p-8 w-[50rem]">
+                        {/* Image */}
+                        <img
+                          src={data.img}
+                          alt="Modal Image"
+                          className="mx-auto mb-4"
+                        />
+                        {/* Text */}
+                        <p className="text-gray-700 mb-4">{data.description}</p>
+                        {/* Close button */}
+                        <button
+                          className="bg-[#270405] hover:bg-black text-white font-bold py-2 px-4 rounded"
+                          onClick={() => setShowModal(false)}
+                        >
+                          Close
+                        </button>
+                      </div>
+                    </div>
+                  )}
+                  <div>
+                    <div
+                      className={`text-[6rem] w-full pl-[25rem] tracking-[1rem] italic text-black ${poppins}`}
+                    >
+                      {data.title}
+                    </div>
+                    <div className="flex gap-[4rem]">
+                      <img
+                        onClick={() => setShowModal(true)}
+                        className="min-w-[33rem] h-[10rem]"
+                        src={data.img}
+                      />
+                      <div
+                        className={`min-w-[18rem] text-[0.8rem] leading-[30px] self-end ${poppins}`}
+                      >
+                        {data.description}
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </ScrollContainer>
+        </div>
+        <div className="scrollbar-hide overflow-y-scroll  flex gap-3 ">
+          {cards.map((data, index) => {
+            return (
+              <div
+                className=" flex lg:hidden min-w-[min(22rem,100vw)] flex-col justify-center mt-12 "
+                key={index}
+              >
+                {showModal && (
+                  <div
+                    className={
+                      showModal
+                        ? `fixed top-0 left-0 right-0 bottom-0 bg-gray-500 bg-opacity-50 z-[1000] flex justify-center items-center `
+                        : `hidden`
+                    }
+                    style={{ overflowY: "hidden" }}
+                  >
+                    <div className="bg-white rounded-lg base:p-3 lg:p-8 w-[50rem]">
+                      {/* Image */}
+                      <img
+                        src="/testimonial.png"
+                        alt="Modal Image"
+                        className="mx-auto mb-4"
+                      />
 
-											{/* Text */}
-											<p className="text-gray-700 mb-4 ">
-												{" "}
-												{data.description}
-											</p>
+                      {/* Text */}
+                      <p className="text-gray-700 mb-4 "> {data.description}</p>
 
-											{/* Close button */}
-											<button
-												className="bg-[#270405] hover:bg-black text-white font-bold py-2 px-4 rounded"
-												onClick={() => setShowModal(false)}
-											>
-												Close
-											</button>
-										</div>
-									</div>
-								)}
-								<div className="text-[2.5rem] text-black self-end justify-self-end tracking-[0.7rem] italic capitalize">
-									CAROLYN
-								</div>
-								<div className="flex flex-col mx-6 gap-[1rem]">
-									<img
-										onClick={() => setShowModal(true)}
-										className="object-cover h-[10rem]"
-										src="/testimonial.png"
-									/>
-									<div className=" text-[1.3rem] text-black self-center justify-self-center leading-[30px] ">
-										Using edge cutting technology to provide
-										extremely reliable service Using edge
-										cutting technology to provide extremely
-										reliable service Using edge cutting
-										technology.
-									</div>
-								</div>
-							</div>
-						)
-					})}
-					{/* cards mobile */}{" "}
-				</div>
+                      {/* Close button */}
+                      <button
+                        className="bg-[#270405] hover:bg-black text-white font-bold py-2 px-4 rounded"
+                        onClick={() => setShowModal(false)}
+                      >
+                        Close
+                      </button>
+                    </div>
+                  </div>
+                )}
+                <div className="text-[2.5rem] text-black self-end justify-self-end tracking-[0.7rem] italic capitalize">
+                  CAROLYN
+                </div>
+                <div className="flex flex-col mx-6 gap-[1rem]">
+                  <img
+                    onClick={() => setShowModal(true)}
+                    className="object-cover h-[10rem]"
+                    src="/testimonial.png"
+                  />
+                  <div className=" text-[1.3rem] text-black self-center justify-self-center leading-[30px] ">
+                    Using edge cutting technology to provide extremely reliable
+                    service Using edge cutting technology to provide extremely
+                    reliable service Using edge cutting technology.
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+          {/* cards mobile */}{" "}
+        </div>
 
-				<div className="w-full mt-[10rem] hidden lg:flex justify-between">
-					<div className=" pl-8 z-[1000]">
-						<CustomButton
-							text={"DISCOVER"}
-							color={"black"}
-							href={"#contact"}
-						/>
-					</div>
-					<div
-						className={`flex  gap-[3rem] text-black mr-16 ${poppins}`}
-					>
-						<div className="text-[2rem] italic underline ">
-							PREV
-						</div>
-						<div className="text-[2rem] italic underline ">
-							NEXT
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	)
-}
+        <div className="w-full mt-[10rem] hidden lg:flex justify-between">
+          <div className=" pl-8 z-[1000]">
+            <CustomButton text={"DISCOVER"} color={"black"} href={"#contact"} />
+          </div>
+          <div className={`flex  gap-[3rem] text-black mr-16 ${poppins}`}>
+            <div className="text-[2rem] italic underline ">PREV</div>
+            <div className="text-[2rem] italic underline ">NEXT</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Testimonials
+export default Testimonials;
