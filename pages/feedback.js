bf61cb7508575d1914b7010d273d5e0c703717ba
ayan <ayan.websleak@gmail.com>
2023-05-12 19:08:29 +0530

    ayan commit - mobile services

--- pages/feedback.js
+++ pages/feedback.js
@@ -1,19 +1,12 @@
-import React from 'react'
-
-import localFont from "next/font/local"
-const golden = localFont({
-	src: "../styles/font/golden/golden.woff2",
-	variable: "--font-golden",
-})
-const poppins = localFont({
-	src: "../styles/font/poppins/Poppins-ExtraBold.woff2",
-	variable: "--font-poppins",
-})
+import React from "react";
 
 const Feedback = () => {
   return (
-    <div className={`${golden.className} base:text-[3rem] lg:text-[min(5vw,6rem)] flex items-center justify-center w-full py-[15rem]`}>Coming Soon</div>
-  )
-}
+    // <div className={`${golden.className} base:text-[3rem] lg:text-[min(5vw,6rem)] flex items-center justify-center w-full py-[15rem]`}>Coming Soon</div>
+    <>
+      <Feedback />
+    </>
+  );
+};
 
-export default Feedback
+export default Feedback;
--- pages/rentalandleaseservices.js
+++ pages/rentalandleaseservices.js
@@ -1,13 +1,28 @@
 import React from "react";
 import Rental from "../components/Rental.js";
 import RentalServices from "../components/RentalServices.js";
+import Feedback from "../components/Feedback.js";
+import ServicesRentalMobile from "../components/ServicesRentalMobile.js";
+import { useState, useEffect } from "react";
 
 const rentalandleaseservices = () => {
+  const [isMobileDevice, setIsMobileDevice] = useState(false);
+
+  useEffect(() => {
+    if (window) {
+      if (window.innerWidth < 1020) {
+        return setIsMobileDevice(true);
+      }
+      return setIsMobileDevice(false);
+    }
+  }, []);
   return (
     <div className="w-full flex justify-center">
       <div className="w-[95%] ">
         <Rental />
-        <RentalServices />
+        {!isMobileDevice ? <RentalServices /> : <ServicesRentalMobile />}
+
+        <Feedback />
       </div>
     </div>
   );
