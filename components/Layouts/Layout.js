5682598c247a0edf8f0a5e90883f7cc0d22ae753
abdul-ahad12 <abdulkouzer@gmail.com>
2023-07-15 13:07:54 +0530

    init

--- components/Layouts/Layout.js
+++ components/Layouts/Layout.js
@@ -1,54 +1,54 @@
 /** @format */
 
-import { useRouter } from "next/router"
-import React, { useEffect, useRef } from "react"
-import Navbar from "../Navbar"
-import Footer from "../Footer"
+import { useRouter } from "next/router";
+import React, { useEffect, useRef } from "react";
+import Navbar from "../Navbar";
+import Footer from "../Footer";
 
-import localFont from "next/font/local"
-import LoadingScreen from "../LoadingScreen"
-import ExampleComponent from "../ExampleComponent"
-import { gsap } from "gsap"
-import SineWave from "../sine"
-import { Lenis } from "@studio-freight/react-lenis"
-import { Cursor } from "../Cursor"
+import localFont from "next/font/local";
+import LoadingScreen from "../LoadingScreen";
+import ExampleComponent from "../ExampleComponent";
+import { gsap } from "gsap";
+import SineWave from "../sine";
+import { Lenis } from "@studio-freight/react-lenis";
+import { Cursor } from "../Cursor";
 
 // Font files can be colocated inside of `pages`
 const golden = localFont({
-	src: "../../styles/font/golden/golden.woff2",
-	variable: "--font-golden",
-})
+  src: "../../styles/font/golden/golden.woff2",
+  variable: "--font-golden",
+});
 
-const Layout = props => {
-	const mainContainerRef = useRef(null)
-	const router = useRouter()
-	const path = router.pathname.split("?")[0]
+const Layout = (props) => {
+  const mainContainerRef = useRef(null);
+  const router = useRouter();
+  const path = router.pathname.split("?")[0];
 
-	const easeInOutQuart = x => {
-		return x < 0.5
-			? 8 * x * x * x * x
-			: 1 - Math.pow(-2 * x + 2, 4) / 2
-	}
+  const easeInOutQuart = (x) => {
+    return x < 0.5 ? 8 * x * x * x * x : 1 - Math.pow(-2 * x + 2, 4) / 2;
+  };
 
-	return (
-		<Lenis
-			root
-			options={{
-				lerp: 0.05,
-				// easing: t => easeInOutQuart(t),
-			}}
-		>
-			<div>
-				<main>
-					<Navbar />
-					<Cursor />
-					<ExampleComponent />
-					{props.children}
-					<Footer />
-				</main>
-			</div>
-		</Lenis>
-	)
-}
+  return (
+    <Lenis
+      root
+      options={{
+        lerp: 0.05,
+        // easing: t => easeInOutQuart(t),
+      }}
+    >
+      <div>
+        <main>
+          {router.pathname !== "/service/landing" && <Navbar />}
+          {router.pathname !== "/service/landing" && <Cursor />}
+          {router.pathname !== "/service/landing" && <ExampleComponent />}
 
-export default Layout
+          <div className="cursor-default">{props.children}</div>
+          {router.pathname !== "/service/landing" && <Footer />}
+
+        </main>
+      </div>
+    </Lenis>
+  );
+};
+
+export default Layout;
