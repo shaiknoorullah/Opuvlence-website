84f169bf1a2adb96c0b1b54b3122a1aa650328f7
ayan <ayan.websleak@gmail.com>
2023-07-16 03:03:11 +0530

    advantages mobile component

--- components/newlanding/Advantages.js
+++ components/newlanding/Advantages.js
@@ -2,9 +2,159 @@ import React from "react";
 import Image from "next/image";
 
 // importing images
-// import Right from "../public/embrace/right.png";
+// desktop ticks images
 import Right from "../../public/embrace/landingright.png";
 import Wrong from "../../public/embrace/landingwrong.png";
+// mobile ticks images
+import BaseRight from "../../public/landingimages/baseright.png";
+import BaseWrong from "../../public/landingimages/basewrong.png";
+
+const BaseAdvantage = () => {
+  const opuvlence = [
+    {
+      id: "1",
+      tittle: "Rent Collection",
+      img: BaseRight,
+    },
+    {
+      id: "2",
+      tittle: "Home Maintenance",
+      img: BaseRight,
+    },
+    {
+      id: "3",
+      tittle: "Best Rent as per the Market",
+      img: BaseRight,
+    },
+    {
+      id: "4",
+      tittle: "More Tenant Options",
+      img: BaseRight,
+    },
+    {
+      id: "5",
+      tittle: "Tenants Background Check",
+      img: BaseRight,
+    },
+    {
+      id: "6",
+      tittle: "Home Interiors",
+      img: BaseRight,
+    },
+    {
+      id: "7",
+      tittle: "Rental Guarantee",
+      img: BaseRight,
+    },
+    {
+      id: "8",
+      tittle: "Tenant Replacement",
+      img: BaseRight,
+    },
+    {
+      id: "9",
+      tittle: "Personal Relationship Manager",
+      img: BaseRight,
+    },
+  ];
+  // others array
+  const other = [
+    {
+      id: "1",
+      tittle: "Rent Collection",
+      img: BaseWrong,
+    },
+    {
+      id: "2",
+      tittle: "Home Maintenance",
+      img: BaseWrong,
+    },
+    {
+      id: "3",
+      tittle: "Best Rent as per the Market",
+      img: BaseWrong,
+    },
+    {
+      id: "4",
+      tittle: "More Tenant Options",
+      img: BaseWrong,
+    },
+    {
+      id: "5",
+      tittle: "Tenants Background Check",
+      img: BaseWrong,
+    },
+    {
+      id: "6",
+      tittle: "Home Interiors",
+      img: BaseWrong,
+    },
+    {
+      id: "7",
+      tittle: "Rental Guarantee",
+      img: BaseWrong,
+    },
+    {
+      id: "8",
+      tittle: "Tenant Replacement",
+      img: BaseWrong,
+    },
+    {
+      id: "9",
+      tittle: "Personal Relationship Manager",
+      img: BaseWrong,
+    },
+  ];
+  return (
+    <div className="md:hidden base:flex  gap-[3.5vw]  flex-col pt-[50px] ">
+      {/* <div className="flex flex-row gap-[40px] w-[100vw]  px-3  overflow-x-scroll "> */}
+      <div
+        className="grid grid-cols-2 gap-[min(19vw,82px)]
+      "
+      >
+        {/* opuvlence */}
+        <div className="flex flex-col items-center gap-[20px]  text-[#270405] font-semibold rounded-[15px] py-[40px] px-[20px]">
+          <div className={`text-[5.62vw] leading-[8.43vw]  `}>opuvlence</div>
+
+          {opuvlence.map((item, idx) => {
+            return (
+              <div
+                className="flex flex-col-reverse gap-[3.44vw] items-center"
+                key={idx}
+              >
+                <div className="text-[3.7vw] leading-[5.74vw]  tracking-[0.01em] font-medium">
+                  {item.tittle}
+                </div>
+                <Image src={item.img} alt="pic" />
+              </div>
+            );
+          })}
+        </div>
+
+        {/* others */}
+        <div className="flex flex-col items-center gap-[20px]   rounded-[15px] py-[40px] px-[20px]">
+          <div className={`text-[5.62vw] leading-[8.43vw] text-[black] `}>
+            other
+          </div>
+          {other.map((item, idx) => {
+            return (
+              <div
+                className="flex flex-col-reverse justify-center gap-[3.44vw] items-center text-black"
+                key={idx}
+              >
+                <div className="text-[3.7vw] leading-[5.74vw] text-justify tracking-[0.01em] ">
+                  {item.tittle}
+                </div>
+                <Image src={item.img} alt="pic" />
+              </div>
+            );
+          })}
+        </div>
+        {/* </div> */}
+      </div>
+    </div>
+  );
+};
 
 const Benefits = () => {
   const Advantage = [
@@ -61,9 +211,11 @@ const Benefits = () => {
     },
   ];
   return (
-    <div className="flex flex-col">
-      <div className="text-[min(1.8vw,35px)] pb-[min(8.1vw,170px)]"></div>
-      <div className="flex flex-col gap-[min(4vw,80px)]">
+    <div className="flex flex-col justify-center items-start">
+      <div className="text-[min(1.8vw,35px)] pb-[min(8.1vw,170px)]">
+        lkfnwdf/lADN
+      </div>
+      <div className="flex flex-col  gap-[min(4.2vw,80px)]">
         {Advantage.map((item, index) => {
           return (
             <div key={index} className="flex flex-col   text-[#270405] ">
@@ -133,16 +285,21 @@ const Righttick = () => {
     },
   ];
   return (
-    <div className="w-[min(20.8vw,400px)] flex flex-col justify-center items-center rounded-lg bg-[#270405] p-[20px]">
+    <div className="w-[min(20.8vw,400px)] flex flex-col justify-center  items-center rounded-lg bg-[#270405] ">
       {/* <Image src={Right} /> */}
-      <div className="text-[min(1.8vw,35px)] pb-[min(4.5vw,86.4px)]">
+      <div className="text-[min(1.8vw,35px)] pb-[min(4.1vw,80px)]">
         Opuvlence
       </div>
-      <div className="flex flex-col gap-[min(4.5vw,86.4px)] ">
+      <div className="flex flex-col gap-[min(4.1vw,80px)] ">
         {tableContent.map((item, index) => {
           return (
             <div className="ite">
-              <Image key={index} src={item.img1} alt="Righttick" />
+              <Image
+                className="w-[min(1.9vw,38px)]"
+                key={index}
+                src={item.img1}
+                alt="Righttick"
+              />
             </div>
           );
         })}
@@ -213,11 +370,16 @@ const WrongTick = () => {
       <div className="text-[min(1.8vw,35px)] text-[#270405] pb-[min(4.1vw,80px)]">
         Other Companies
       </div>
-      <div className="flex flex-col  gap-[min(4.5vw,86.4px)] ">
+      <div className="flex flex-col  gap-[min(4.2vw,80px)] ">
         {tableContent.map((item, index) => {
           return (
             <div className="ite">
-              <Image key={index} src={item.img1} alt="Lefttick" />
+              <Image
+                className="w-[min(1.9vw,38px)]"
+                key={index}
+                src={item.img1}
+                alt="Lefttick"
+              />
             </div>
           );
         })}
@@ -298,7 +460,7 @@ const Advantages = () => {
         <div className="text-[#270405] font-bold text-[min(2.3vw,45px)]">
           Advantages
         </div>
-        <div className="flex w-full  justify-between">
+        <div className="base:hidden md:grid grid-cols-3 place-content-center">
           {/* <div className="mt-[30px]"> */}
           <Benefits />
           {/* </div> */}
@@ -307,6 +469,9 @@ const Advantages = () => {
           <WrongTick />
 
           <div>{/* <Righttick /> */}</div>
+        </div>
+        <div>
+          <BaseAdvantage />
         </div>
         {/* for mobile */}
         {/* <BaseEmbrace /> */}
