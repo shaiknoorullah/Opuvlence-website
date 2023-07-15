875bf44525749f074eaee192cf4a506f3ce2837e
abdul-ahad12 <abdulkouzer@gmail.com>
2023-07-15 13:09:13 +0530

    made a new folder in componenet fr  new landing cp

--- pages/service/landing.js
+++ pages/service/landing.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Hero from '../../components/newlanding/Hero'
 
 const Landing = () => {
   return (
-    <div>Landing</div>
+    <div>
+      <Hero />
+    </div>
   )
 }
 
